b68d6387852027a6a7075405ad4885cb44ecef7d
apofis96 <32576664+apofis96@users.noreply.github.com>
2017-10-08 20:25:20 +0300

    Lab7

---
+++ Chap1/Lab 1.7.3 (1)/Lab 1.7.3 (1)/Lab1731.cpp
@@ -0,0 +1,19 @@
+// Lab1731.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	float a = 2.3, b = 2.3, c = 2.123456, d = 2.123456, f = 2.123456;
+	cout << a <<endl;
+	cout << fixed << setprecision(2) << b << endl;
+	cout << setprecision(6) << c << endl;
+	cout << setprecision(2) << d << endl;
+	cout << setprecision(0) << f << endl;
+    return 0;
+}
+
