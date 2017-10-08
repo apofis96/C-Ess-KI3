ab621fc59e36705be1cf90e61f2ed876ef6a6fc5
apofis96 <32576664+apofis96@users.noreply.github.com>
2017-10-08 20:25:34 +0300

    Lab8

---
+++ Chap1/Lab 1.7.4 (2)/Lab 1.7.4 (2)/Lab1742.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int x, y;
+	float a, b;
+	cout << "x:";
+	cin >> x;
+	cout << "y:";
+	cin >> y;
+	a = x;
+	b = y;
+	if (1.0 / a == 1.0 / b) { cout << "Results are equal (by 0.000001 epsilon)\n"; }
+	else { cout << "Results are not equal (by 0.000001 epsilon)\n"; }
+    return 0;
+}
+
