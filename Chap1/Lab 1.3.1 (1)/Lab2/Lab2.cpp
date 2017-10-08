776f2db1da76e6c86c740c27b52bc74e7e07afd3
apofis96 <32576664+apofis96@users.noreply.github.com>
2017-10-08 20:19:35 +0300

    Lab2

---
+++ Chap1/Lab 1.3.1 (1)/Lab2/Lab2.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+int main()
+{
+	int v = 10800; // 3*60*60
+	int z = 3 * 60;// This is a variable to hold the value of 3 minutes in seconds
+	int zz = 5 * 60;// This is a variable to hold the value of 5 minutes in seconds
+	float pi = 3.141526; //This is the value of pi
+	std::cout << "result: " << v << std::endl; // here we print the v value
+}
