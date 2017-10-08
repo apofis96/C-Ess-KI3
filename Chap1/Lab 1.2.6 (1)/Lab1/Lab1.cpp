5af5fd4e913d142bb4dcfea1c4f5d9167c017df1
apofis96 <32576664+apofis96@users.noreply.github.com>
2017-10-08 20:05:28 +0300

    Lab1

---
+++ Chap1/Lab 1.2.6 (1)/Lab1/Lab1.cpp
@@ -0,0 +1,13 @@
+// Lab1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main(void) {
+	cout << "Welcome back";
+	cout << endl;
+	cout << "It's me, your first prog\nram.";
+	return 0;
+}
+
