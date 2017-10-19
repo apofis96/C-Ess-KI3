a4f8d596da04c9d2953501f609281f4c6ac4d492
apofis96 <32576664+apofis96@users.noreply.github.com>
2017-10-19 21:01:42 +0300

    Lab10

---
+++ Chap2/Lab 2.1.5 (1)/Lab 2.1.5 (1)/Lab2151.cpp
@@ -0,0 +1,36 @@
+// Lab2151.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(void)
+{
+	int year;
+	string s;
+
+	cout << "Enter a year: ";
+	cin >> year;
+		// Insert your code here
+	if (year < 1582)
+	{
+		s = "Non Gregorian";
+	}
+	else 
+	{
+		if (year % 4 == 0 && year % 100 != 0) 
+		{
+			s = "leap year";
+		}
+		else if (year % 400 == 0) 
+		{
+			s = "leap year";
+		}
+		else s = "common year";
+	}
+	cout << s;
+	return 0;
+}
+
